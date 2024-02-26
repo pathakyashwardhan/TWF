ecdd8cdab2d226314f5d103018cd5beb59fcbd11
yashwardhan pathak <pathakyashwardhan54@gmail.com>
2024-02-26 14:25:04 +0530

    finals

--- translation.routes.js
+++ translation.routes.js
@@ -5,4 +5,5 @@ import { translations } from "./translation.controller.js";
 
 const router = Router();
 router.route("/tofrench").get(translations).post(translations);
+router.get("/tofrench/:text", translations);
 export default router
